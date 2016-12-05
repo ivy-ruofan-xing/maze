64d8833fe2dcdefc329a3639e183628702013234
Ivy Xing <r5xing@uwaterloo.ca>
2016-12-05 01:48:52 -0500

    Adding project code

---
+++ Project/main/portal.h
@@ -0,0 +1,11 @@
+#ifndef PORTAL_H_INCLUDED
+#define PORTAL_H_INCLUDED
+
+int portalPos[2][2] = {{0, 0}, {0, 0}};
+
+void updatePortalPosition();  //new positions of the portals for when it needs to be moved and redrawn
+
+int checkIfOnPortal();  //move the ball to the next portal if it goes through
+
+
+#endif
---
+++ Project/main/wirechange.h
@@ -0,0 +1,12 @@
+#ifndef WIRECHANGE_H_INCLUDED
+#define WIRECHANGE_H_INCLUDED
+
+#include <Wire.h>
+
+void WireInit();  //initialize the wire
+void WireWrite(int address, uint8_t value);  //writes one value
+void WireWriteTwo(int address, uint8_t value, uint8_t value2);  //writes two values
+void GetValueArray(int address, uint32_t *ValArray, uint8_t len);  //creates an array of values
+
+#endif
+
